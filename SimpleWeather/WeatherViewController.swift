5405db40b6af0c0df23e0b3c54c6120112de9e39
chihyeon.hwang <email@example.com>
2024-12-10 16:54:04 +0900

    Final

--- SimpleWeather/WeatherViewController.swift
+++ SimpleWeather/WeatherViewController.swift
@@ -9,21 +9,32 @@ import UIKit
 
 class WeatherViewController: UIViewController {
 
+    //데이터를 받기 위해 설정
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var weatherImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    
+    let cities = ["Seoul", "Tokyo", "New York", "Seattle"]
+    let weathers = ["cloud.fill", "sun.max.fill", "wind", "cloud.sun.rain.fill"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func changeButtonTapped(_ sender: Any) {
+        
+        cityLabel.text = cities.randomElement()
+        
+        //랜덤 날씨 이미지
+        //.alwaysOriginal: 원래의 색 렌더링
+        //.alwaysTemplate: 형태만 쓰고, 색은 UI 컨퍼런트 사용
+        let imageName = weathers.randomElement()!
+        weatherImageView.image = UIImage(systemName: imageName)?.withRenderingMode(.alwaysOriginal)
+    
+        let randomTemp = Int.random(in: 1..<30)
+        temperatureLabel.text = "\(randomTemp)°"
+        
+        print("도시, 온도, 날씨 이미지 변경하자")
     }
-    */
-
+    
 }
